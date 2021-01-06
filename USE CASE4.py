4262b6d64c5e14f155aa1f376fc76d37a9b746bf
ANJI1196 <76936911+ANJI1196@users.noreply.github.com>
2021-01-06 20:19:35 +0530

    Add files via upload

---
+++ USE CASE4.py
@@ -0,0 +1,18 @@
+#MENU APP:
+while True:
+    print("MY TO DO APP")
+    print("==============")
+    print("1. Add Task")
+    print("2. View All Tasks")
+    print("0. To Exit")
+    option=int(input("Choose Option:"))
+
+    if option==1:
+        n1=input("Enter Task Name:")
+        print("Task added")
+    elif option==2:
+        n1=input("Enter Task Name:")
+        print("Task added")
+    elif option==0:
+        print("Bye")
+        break
